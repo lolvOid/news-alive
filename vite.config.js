b674e32e4bb30daebf647840f264f73dbc8c1eff
lolvOid <linhtet1998itachi@gmail.com>
2023-05-11 21:07:45 +0100

    Fixed Vite Config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vue from '@vitejs/plugin-vue';
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
 
-const env = loadEnv("development",process.env)
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
