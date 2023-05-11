851d4bdf5a06e5551ec7f1e5b523891b4864e9e0
lolvOid <linhtet1998itachi@gmail.com>
2023-05-11 21:06:47 +0100

    Fixed Vite Config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig, loadEnv ,loadEnv} from 'vite';
+import { defineConfig, loadEnv} from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
