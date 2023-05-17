49f2495deab7bade50f9872c15483ff1483dd87a
lolvOid <linhtet1998itachi@gmail.com>
2023-05-17 09:03:12 +0100

    Updated: Headline

--- src/components/HeadlineCard.vue
+++ src/components/HeadlineCard.vue
@@ -1,86 +1,198 @@
 <template>
+  <v-card v-if="data" class="rounded-0 mb-2 fill-height" density="compact" variant="elevated" elevation="2" rounded="lg">
 
-    <v-col v-for="v in data" cols="12" md="4" sm="6" lg="3" xl="2">
-      <v-card border class="rounded-0 mb-2 fill-height" density="comfortable" :prepend-avatar="formatIcon(v.url)"
-        :title="v.title" variant="text">
-        <v-img :src="v.urlToImage" height="128" cover></v-img>
-        <template v-slot:subtitle>
-          <v-icon icon="mdi:mdi-clock-outline"></v-icon>
-          {{ formatDuration(v.publishedAt) }}
-        </template>
-        <v-card-text>
-          {{ v.description ? v.description.substring(0, 100) + '... ' : '' }}
-          <router-link :to="{
-            name:'article.show',
-            params:{
-              id:v.id,
-            }
-          }" custom v-slot="{ navigate }">
-            <button role="link" class="font-weight-bold"   @click="navigate">
-              Read More
-            </button>
+    <v-img :lazy-src="data.urlToImage ? data.urlToImage : placeholderImage" class="rounded-lg"
+      :src="data.urlToImage ? data.urlToImage : placeholderImage" height="200" cover
+      gradient="to top, rgba(0,0,0,0.5), rgba(0,0,0,0)">
+      <template v-slot:placeholder>
+        <div class="d-flex align-center justify-center fill-height">
+          <v-progress-circular color="grey-lighten-4" indeterminate></v-progress-circular>
+        </div>
+      </template>
+      <v-toolbar  color="rgba(0, 0, 0, 0)">
+        <template v-slot:prepend>
+          <router-link class="text-decoration-none text-black" :to="{
+            name: 'home.search',
+            query: {
+                sources:data.source.id?data.source.id:''
+            },
+          }">
+          <v-avatar size="36">
+            
+            <img :src="data.icon" height="36"  />
+          </v-avatar>
+    
           </router-link>
-
-        </v-card-text>
-
-        <!-- <template v-slot:actions>
-          <v-btn  class="floating" color="black" variant="text">Read More</v-btn>
          
-        </template> -->
-      </v-card>
-    </v-col>
+        </template>
 
+        <template v-slot:append>
+          <v-btn icon variant="text" color="white">
+            <v-icon icon="mdi:mdi-dots-vertical"></v-icon>
+
+            <v-menu activator="parent">
+              <v-list rounded="xl">
+                <v-list-item value="detail" key="detail" prepend-icon="mdi:mdi-page-next" :to="{
+                  name: 'article.show',
+                  params: {
+                    id: data.id,
+                  },
+                }">
+                  Read
+                </v-list-item>
+                <v-list-item value="edit" key="edit" prepend-icon="mdi:mdi-pencil-box"
+                  @click="; (editDialog = true), setId(data.id)">
+                  <template v-slot:append v-if="data.title.length > 255">
+                    <v-icon color="orange" icon="mdi:mdi-alert-box"></v-icon>
+                  </template>
+                  Edit Title
+                </v-list-item>
+
+                <v-list-item value="goToSource" key="goToSource" prepend-icon="mdi:mdi-link-variant" :href="data.url"
+                  target="_blank">
+                  Original Source
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-btn>
+        </template>
+      </v-toolbar>
+    </v-img>
+
+
+    <v-card-title v-if="highlightText" v-html="formatHighlightText(processedContent( data.title).substring(0, 30))" class="pb-0 overflow-hidden">
+    </v-card-title>
+
+    <v-card-title v-else class="overflow-hidden pb-0">
+      <router-link class="text-decoration-none text-black" :to="{
+        name: 'article.show',
+        params: {
+          id: data.id,
+        },
+      }">
+        <v-tooltip :text="data.title" location="top">
+          <template v-slot:activator="{ props }">
+
+            <span v-bind="props" class="overflow-hidden pb-0"> {{ processedContent( data.title).substring(0, 50) }}...</span>
+          </template>
+        </v-tooltip>
+
+      </router-link>
+    </v-card-title>
+
+    <v-card-text class="text-caption pb-0">
+
+      <v-icon icon="mdi:mdi-clock-outline"></v-icon>
+      <v-tooltip location="top" :text="formatDate(data.publishedAt)">
+        <template v-slot:activator="{ props }">
+          <span v-bind="props"> {{ formatDuration(data.publishedAt) }} |</span>
+
+        </template>
+      </v-tooltip>
+      <v-chip class="ma-2" size="small" :to="{
+        name:'home.search',
+        query:{
+          'sources':data.source.id
+        }
+      }">
+        {{ data.source.name }}
+      </v-chip>
+
+    </v-card-text>
+
+    <v-card-text>
+      <span v-if="highlightText" v-html="formatHighlightText(processedContent(data.description).substring(0, 50))"></span>
+      <span v-else v-html="processedContent(data.description).substring(0, 50)+'...'">
+      
+       
+      </span>
+      <router-link :to="{
+          name: 'article.show',
+          params: {
+            id: data.id,
+          },
+        }" custom v-slot="{ navigate }">
+        <button role="link" class="font-weight-bold" @click="navigate">
+          See More
+        </button>
+      </router-link>
+    </v-card-text>
+
+    
+  </v-card>
+
+  <EditTitleDialog :id=currentHeadlineId v-model="editDialog" @dialogClosed="editDialog = false" />
 </template>
 
 <script>
 import moment from 'moment'
+import EditTitleDialog from './EditTitleDialog.vue'
 export default {
   name: 'HeadlineCard',
-  props: ['data'],
-  
+  props: {
+    data: null,
+    highlightText: {
+      type: String,
+      default: '',
+    },
+  },
 
+  components: { EditTitleDialog },
   data() {
-    return {}
+    return {
+      editDialog: false,
+      currentHeadlineId: '',
+      placeholderImage:
+        'https://dummyimage.com/400x300.jpg?text=Image%20Not%20Available',
+    }
   },
   methods: {
-  
+    setId(value) {
+      this.currentHeadlineId = value
+    },
+    formatHighlightText(value) {
+  if (value) {
+    return value.replace(
+      new RegExp(this.escapeRegExp(this.highlightText), 'gim'),
+      (match) => `<span class='highlight'>${match}</span>`
+    );
+  }
+  return '';
+},
+ escapeRegExp(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+},
     formatDate(date) {
       return moment(date).format('DD/MM/YYYY')
     },
     formatTime(time) {
       return moment(time).format('HH:MMa')
     },
-    formatDuration(date) {
-      const now = moment()
-      const past = moment(date)
-      const diff = moment.duration(now.diff(past))
-      const minDuration = diff.asMinutes().toFixed(0)
-      const dayDuration = diff.asDays().toFixed(0)
-      const timeDuration = diff.asHours().toFixed(0)
-      const yearDuration = diff.asYears().toFixed(0)
-      const monthDuration = diff.asMonths().toFixed(0)
-      let format = ''
-      if (minDuration >= 60) {
-        if (timeDuration >= 24) {
-          format = dayDuration + 'd ago'
-          if (dayDuration > 31) {
-            format = monthDuration + 'mth ago'
-            if (monthDuration > 12) {
-              format = yearDuration + 'y ago'
-            }
-          }
-        }
-      } else {
-        format = minDuration + 'm ago'
-      }
-      return format
-    },
-    formatIcon(url = '') {
- 
-      const origin = new URL(url).origin
+    processedContent(content) {
 
-      return `https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${origin}&size=128`
+return content ? content.replace(/<a href="(.*?)">(.*?)<\/a>/g, '<a :href="$1">$2</a>') : "";
+
+},
+    formatDuration(date) {
+      const diff = moment.duration(moment().diff(moment(date)))
+      const duration =
+        diff.asMinutes() < 60
+          ? diff.asMinutes().toFixed(0) + 'm'
+          : diff.asHours() < 24
+            ? diff.asHours().toFixed(0) + 'h'
+            : diff.asDays() < 31
+              ? diff.asDays().toFixed(0) + 'd'
+              : diff.asMonths() < 12
+                ? diff.asMonths().toFixed(0) + 'mo'
+                : diff.asYears().toFixed(0) + 'y'
+      return `${duration} ago`
     },
   },
 }
 </script>
+
+<style type="css">
+.highlight {
+  background-color: yellow;
+}
+</style>
--- src/components/Headlines.vue
+++ src/components/Headlines.vue
@@ -1,20 +1,26 @@
 <template>
+
+ 
   <v-row class="fill-height mb-5" justify="start" align="stretch" id="container">
-    <HeadlineCard :data="data"  />
+    <v-col v-for="v in data" cols="12" md="6" sm="6" lg="4" xl="3">
+      <HeadlineCard :data="v" :highlightText="highlightText" />
+    </v-col>
+    
   </v-row>
+
+  
 </template>
 
 <script>
 import HeadlineCard from './HeadlineCard.vue';
+
 export default {
   name: "Headlines",
   components: { HeadlineCard },
   
-  props: ['data'],
+  props: ['data','highlightText'],
+ 
 
-  methods: {
-    
-  }
 
 }
 </script>
