efdaa8e09fd8180350e363a62e9e85a2e54c5c69
lolvOid <linhtet1998itachi@gmail.com>
2023-05-17 15:10:41 +0700

    Updated Vuex  State

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import store from "./store";
+import FlagIcon from 'vue-flag-icon'
 loadFonts()
 
 const app = createApp(App);
 store.dispatch("fetchHeadlines")
+store.dispatch("createCategories");
+store.dispatch("createCountries");
   
 store.dispatch("fetchSources")
-
+app.use(FlagIcon)
 app.use(router);
 app.use(store);
 app.use(vuetify);
