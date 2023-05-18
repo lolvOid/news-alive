ad87f3ff45e48f5a9f32ccd8fd15d2010d33bb23
lolvOid <linhtet1998itachi@gmail.com>
2023-05-18 09:59:41 +0100

    Added PWA

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 import { createApp } from 'vue'
 import FlagIcon from 'vue-flag-icon'
+import { registerSW } from 'virtual:pwa-register'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import store from './store'
+
+const intervalMS = 60 * 60 * 1000
+
+registerSW({
+  onRegisteredSW(swUrl, r) {
+    r &&
+      setInterval(async () => {
+        if (!(!r.installing && navigator)) return
+
+        if ('connection' in navigator && !navigator.onLine) return
+
+        const resp = await fetch(swUrl, {
+          cache: 'no-store',
+          headers: {
+            cache: 'no-store',
+            'cache-control': 'no-cache',
+          },
+        })
+
+        if (resp?.status === 200) await r.update()
+      }, intervalMS)
+  },
+})
 
 // Create the Vue application and configure plugins
 const app = createApp(App).use(FlagIcon).use(router).use(store).use(vuetify)
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,48 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
+import { VitePWA } from 'vite-plugin-pwa'
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     {
-      name: 'vitest-plugin-beforeall'
+      name: 'vitest-plugin-beforeall',
     },
-
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      workbox: {
+        cleanupOutdatedCaches: false,
+        sourcemap: true,
+        clientsClaim: true,
+        skipWaiting: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'ALIVE',
+        short_name: 'NEWS',
+        description: 'News Reader',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icon_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
     vue({
       reactivityTransform: true,
     }),
